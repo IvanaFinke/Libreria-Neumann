fe2f90a9d7f86e48354d22905370ba10b09470a1
Ivana Maitena Finke <ivanamaitena@gmail.com>
2026-01-11 20:23:06 -0300

    header-responsive-cell

--- LibreriaNeumann/LibreriaNeumann/obj/Debug/net10.0/LibreriaNeumann.AssemblyInfo.cs
+++ LibreriaNeumann/LibreriaNeumann/obj/Debug/net10.0/LibreriaNeumann.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a5b1543d095fc591aae5adb004ee4422840f4b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20aeb42c8ad074b3149806abb1ef77896f490b30")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
