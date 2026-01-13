d54c77b99bebad69d0df4e894d3aa8a852f4e54c
Ivana Maitena Finke <ivanamaitena@gmail.com>
2026-01-12 23:52:08 -0300

    cambio-datos-seccion-pc

--- LibreriaNeumann/LibreriaNeumann/obj/Debug/net10.0/LibreriaNeumann.AssemblyInfo.cs
+++ LibreriaNeumann/LibreriaNeumann/obj/Debug/net10.0/LibreriaNeumann.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9ec09e2c39c6cc44277638a002a9d116b95cfc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5abc1f0468b5c50dfe5b7bd1a07397411eb3a1c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
