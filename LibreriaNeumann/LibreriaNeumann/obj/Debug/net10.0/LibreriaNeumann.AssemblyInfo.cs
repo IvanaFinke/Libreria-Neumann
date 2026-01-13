d9ec09e2c39c6cc44277638a002a9d116b95cfc6
Ivana Maitena Finke <ivanamaitena@gmail.com>
2026-01-12 23:17:17 -0300

    seccion-pc-usuario-datos

--- LibreriaNeumann/LibreriaNeumann/obj/Debug/net10.0/LibreriaNeumann.AssemblyInfo.cs
+++ LibreriaNeumann/LibreriaNeumann/obj/Debug/net10.0/LibreriaNeumann.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7c514f4ea80ecb9ee2f406b4d20fa648267c73b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+154fb1fa96e89b9f498994d2421a1c7885576181")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
