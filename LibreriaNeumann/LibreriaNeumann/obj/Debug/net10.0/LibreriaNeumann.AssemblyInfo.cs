05e1bdbd4d6154037420c22cec84f3514a0cbda0
Ivana Maitena Finke <ivanamaitena@gmail.com>
2026-01-18 20:24:52 -0300

    hero-responsive-animation

--- LibreriaNeumann/LibreriaNeumann/obj/Debug/net10.0/LibreriaNeumann.AssemblyInfo.cs
+++ LibreriaNeumann/LibreriaNeumann/obj/Debug/net10.0/LibreriaNeumann.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97f0b912ef48f6cd1bccabcafc56560a43ac53c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+176baecf66280c104a536abb5d67da65f10be00a")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
