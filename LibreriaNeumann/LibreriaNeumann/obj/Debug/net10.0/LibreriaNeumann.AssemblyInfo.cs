ef1165d8b302e3644e48672f4dc0a04e96cdf41b
Ivana Maitena Finke <ivanamaitena@gmail.com>
2026-01-19 17:44:15 -0300

    footer-responsive-tablet

--- LibreriaNeumann/LibreriaNeumann/obj/Debug/net10.0/LibreriaNeumann.AssemblyInfo.cs
+++ LibreriaNeumann/LibreriaNeumann/obj/Debug/net10.0/LibreriaNeumann.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05e1bdbd4d6154037420c22cec84f3514a0cbda0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8986b9096fb2e3881343daa3686e833f10ab99e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
