e7f5abf2be46226e33e29d6b5ac3b7bdbe06dd05
Ivana Maitena Finke <ivanamaitena@gmail.com>
2026-01-20 16:08:22 -0300

    carrito-responsive-ipads-grande-chico

--- LibreriaNeumann/LibreriaNeumann/obj/Debug/net10.0/LibreriaNeumann.AssemblyInfo.cs
+++ LibreriaNeumann/LibreriaNeumann/obj/Debug/net10.0/LibreriaNeumann.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3879523febf8211bf1f729c5e1c3b8fa05fa68e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c141e041487377be7b92bb2605ee60f8b841090")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaNeumann")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
