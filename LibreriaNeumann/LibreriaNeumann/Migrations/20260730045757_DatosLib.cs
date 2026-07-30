0e72185e6824a6261fe1fe21676d72606e842332
Ivana Maitena Finke <ivanamaitena@gmail.com>
2026-07-30 02:32:18 -0300

    salvando el codigo de datos lib

---
+++ LibreriaNeumann/LibreriaNeumann/Migrations/20260730045757_DatosLib.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibreriaNeumann.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatosLib : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           
+        }
+    }
+}
