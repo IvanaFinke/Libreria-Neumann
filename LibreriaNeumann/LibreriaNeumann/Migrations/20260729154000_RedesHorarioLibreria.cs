ce3515f422d15a8871f963c3b8fc18b45596a9e7
Ivana Maitena Finke <ivanamaitena@gmail.com>
2026-07-30 01:25:03 -0300

    agrega componente admin informacion con redes y horario

---
+++ LibreriaNeumann/LibreriaNeumann/Migrations/20260729154000_RedesHorarioLibreria.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibreriaNeumann.Migrations
+{
+    /// <inheritdoc />
+    public partial class RedesHorarioLibreria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Facebook",
+                table: "Libreria",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "HoraInicio",
+                table: "Libreria",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "HoraSalida",
+                table: "Libreria",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Instagram",
+                table: "Libreria",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tiktok",
+                table: "Libreria",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Facebook",
+                table: "Libreria");
+
+            migrationBuilder.DropColumn(
+                name: "HoraInicio",
+                table: "Libreria");
+
+            migrationBuilder.DropColumn(
+                name: "HoraSalida",
+                table: "Libreria");
+
+            migrationBuilder.DropColumn(
+                name: "Instagram",
+                table: "Libreria");
+
+            migrationBuilder.DropColumn(
+                name: "Tiktok",
+                table: "Libreria");
+        }
+    }
+}
